53df4ea7d0e3615e9d57c18c2297ea2655fa8b11
Kyungho Jeon <kyunghoj@jeju.(none)>
2012-10-24 15:05:57 -0400

    Class name changed. Some progresses.

---
+++ src/edu/buffalo/cse/jeju/mapred/RepartitionEquiJoin.java
@@ -0,0 +1,131 @@
+/**
+ * 
+ */
+package edu.buffalo.cse.jeju.mapred;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+
+
+/**
+ * @author kyunghoj
+ *
+ */
+public class RepartitionEquiJoin {
+
+	// left table "L"'s filename
+	private static String L = "L";
+	// right table "R"'s filename
+	private static String R = "R";
+    
+	private static final Log LOG = LogFactory.getLog(RepartitionEquiJoin.class);
+	
+	public static class Repartitioner 
+		extends Partitioner<Text, Text> {
+		public int getPartition(Text key, Text value, int numPartitions) {
+			String joinKey = null;
+			try {
+				String strKey = key.toString();
+				joinKey = strKey.split(":")[1];
+				LOG.debug("Composite key split: " + strKey.split(":")[0] + ", " + strKey.split(":")[1]);
+			} catch (NullPointerException npe) {
+				return 0;
+			}
+			return joinKey.hashCode() % numPartitions;
+		}
+	}
+	
+	public static class RepartitionEquiJoinMapper 
+		extends Mapper<Text, Text, Text, Text> {
+		
+		public static final Log LOG = LogFactory.getLog(RepartitionEquiJoinMapper.class);
+		
+		@Override
+		protected void setup(Context context) {
+			Configuration conf = context.getConfiguration();
+		}
+		
+		public void map(Text key, Text values, Context context) 
+			throws IOException, InterruptedException {
+			
+			String inputfile = null;
+			InputSplit split = context.getInputSplit();
+			
+			if (split instanceof FileSplit) {
+				FileSplit fsplit = (FileSplit) split;
+				inputfile = fsplit.getPath().getName();
+			} else {
+				LOG.info("InputSplit is not FileSplit.");
+				return;
+			}
+			
+			String stringKey = key.toString();
+			Configuration conf = context.getConfiguration();
+			
+			String mapOutKey = null;
+			
+			if (inputfile.endsWith(L)) {
+				LOG.debug("Table L");
+				mapOutKey = "L:" + stringKey;
+			} else if (inputfile.endsWith(R)) {
+				LOG.debug("Table R");
+				mapOutKey = "R:" + stringKey;
+			}
+			
+			// do not filter. 
+			// assume we need all the columns from both tables.
+			context.write(new Text(mapOutKey), values);
+		}
+	}
+	
+	public static class RepartitionEquiJoinReducer 
+		extends Reducer<Text, Text, Text, Text> {
+		
+		private HashMap<String, String> leftRowsBuffer = null;
+		
+		@Override
+		protected void setup(Context context) {
+			Configuration conf = context.getConfiguration();
+			leftRowsBuffer = new HashMap<String, String>();
+		}
+		
+		public void reduce(Text key, Iterable<Text> rows, Context context)
+			throws IOException, InterruptedException {
+			String srcTbl  = key.toString().split(":")[0];
+			String joinKey = key.toString().split(":")[1];
+			boolean isLTable = srcTbl.matches("L");
+			
+			for (Text row : rows) {
+				if (isLTable) {
+					leftRowsBuffer.put(joinKey, row.toString());
+				} else {
+					if (leftRowsBuffer.containsKey(joinKey)) {
+						String result = joinKey + ",<" + 
+								leftRowsBuffer.get(joinKey) + ">, <" + 
+								row.toString() + ">";
+						
+						context.write(null, new Text(result));
+					} else {
+						String result = joinKey + ", <null>, " + "<" + row.toString() + ">"; 
+						context.write(null, new Text(result));
+					}
+				}
+			}
+			
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+	}
+}
