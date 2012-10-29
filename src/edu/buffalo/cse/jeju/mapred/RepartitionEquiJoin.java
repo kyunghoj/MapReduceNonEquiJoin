4405df4863ae10ecd333df11ce8037b75c623527
Kyungho Jeon <kyungho.jeon@gmail.com>
2012-10-29 14:31:12 -0400

    Somehow it works for a simple case. Let's improve this.

--- src/edu/buffalo/cse/jeju/mapred/RepartitionEquiJoin.java
+++ src/edu/buffalo/cse/jeju/mapred/RepartitionEquiJoin.java
@@ -11,14 +11,19 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileSplit;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 
 /**
@@ -50,7 +55,7 @@ public class RepartitionEquiJoin extends Configured implements Tool {
 	}
 	
 	public static class RepartitionEquiJoinMapper 
-		extends Mapper<Text, Text, Text, Text> {
+		extends Mapper<LongWritable, Text, Text, Text> {
 		
 		public static final Log LOG = LogFactory.getLog(RepartitionEquiJoinMapper.class);
 		
@@ -59,7 +64,7 @@ public class RepartitionEquiJoin extends Configured implements Tool {
 			Configuration conf = context.getConfiguration();
 		}
 		
-		public void map(Text key, Text values, Context context) 
+		public void map(LongWritable key, Text values, Context context) 
 			throws IOException, InterruptedException {
 			
 			String inputfile = null;
@@ -73,21 +78,34 @@ public class RepartitionEquiJoin extends Configured implements Tool {
 				return;
 			}
 			
-			String stringKey = key.toString();
+			
+			String line = values.toString();
+			if (DEBUG) {
+				System.err.printf("[Debug] Input line = %s\n", line);
+			}
+			
+			String strKey = line.split(":")[0];
+			String strVals = line.split(":")[1];
+			
+			if (DEBUG) {
+				System.err.printf("[Debug] map key = %s map val = %s\n", strKey, strVals);
+			}
+			
 			Configuration conf = context.getConfiguration();
 			
 			String mapOutKey = null;
 			
 			if (inputfile.endsWith(L)) {
 				LOG.debug("Table L");
-				mapOutKey = "L:" + stringKey;
+				mapOutKey = "L:" + strKey;
 			} else if (inputfile.endsWith(R)) {
 				LOG.debug("Table R");
-				mapOutKey = "R:" + stringKey;
+				mapOutKey = "R:" + strKey;
 			}
 			
 			// do not filter. 
 			// assume we need all the columns from both tables.
+			// values is in a form of "key:values"
 			context.write(new Text(mapOutKey), values);
 		}
 	}
@@ -131,20 +149,16 @@ public class RepartitionEquiJoin extends Configured implements Tool {
 	
 	private static String LEFT_TABLE = "LEFT_TABLE";
 	private static String RIGHT_TABLE = "RIGHT_TABLE";
+	private static String OUTPUT_TABLE = "OUTPUT_TABLE";
 	
 	private static boolean DEBUG = true;
 
 	public int run(String[] args) throws Exception {
 		
-		return 0;
-	}
-	
-	public static void main(String[] args) throws Exception {
-	
-		
 		if (args.length < 2) {
-			System.err.println("Usage: RepartitionEquiJoin Left_Table Right_Table [Configuration file]");
-			return;
+			System.err.printf("Usage: %s <Left_Table> <Right_Table> <Output_Table> [Configuration file]\n",
+					getClass().getSimpleName());
+			return -1;
 		}
 		
 		int i = 0;
@@ -152,7 +166,9 @@ public class RepartitionEquiJoin extends Configured implements Tool {
 		
 		conf.set(LEFT_TABLE, args[i++]);
 		conf.set(RIGHT_TABLE, args[i++]);
-		if (args.length > 2) {
+		conf.set(OUTPUT_TABLE, args[i++]);
+		
+		if (args.length > 3) {
 			conf.addResource(args[i++]);
 		}
 		
@@ -162,9 +178,27 @@ public class RepartitionEquiJoin extends Configured implements Tool {
 			}
 		}
 		
-		Job job = new Job(conf, "RepartitionJoin");
+		Job job = new Job(conf, "Repartition Join");
 		
 		job.setJarByClass(RepartitionEquiJoin.class);
 		
+		FileInputFormat.addInputPath(job, new Path(conf.get(LEFT_TABLE)));
+		FileInputFormat.addInputPath(job, new Path(conf.get(RIGHT_TABLE)));
+		FileOutputFormat.setOutputPath(job, new Path(conf.get(OUTPUT_TABLE)));
+		
+		
+		job.setMapperClass(RepartitionEquiJoinMapper.class);
+		job.setReducerClass(RepartitionEquiJoinReducer.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		
+		return job.waitForCompletion(true) ? 0 : 1;
 	}
+	
+	public static void main(String[] args) throws Exception {
+		int exitCode = ToolRunner.run(new RepartitionEquiJoin(), args);
+		System.exit(exitCode);
+	}
+	
 }
